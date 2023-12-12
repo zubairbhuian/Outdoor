3e17905bd26e23397bbdf0d0d99115a419207ee2
Zubair <74538410+zubairbhuian@users.noreply.github.com>
2023-12-12 12:10:43 +0600

    base

---
+++ lib/app/core/utils/icons.dart
@@ -0,0 +1,16 @@
+class IconsPath {
+  IconsPath._();
+  static const String email = 'assets/icons/email.svg';
+  static const String facebook = 'assets/icons/facebook.svg';
+  static const String google = 'assets/icons/google.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String user = 'assets/icons/user.svg';
+  static const String userFilled = 'assets/icons/user_filled.svg';
+  static const String logout = 'assets/icons/logout.svg'; 
+}
+
+
+class ImagesPath {
+  ImagesPath._();
+  static const String userVactor = 'assets/images/Vectorscreen_person.svg';
+}
